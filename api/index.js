fe4d47f711de10c0c880bb4dfbfb3af4d00efdf7
varsan <support@ye.ua>
2020-04-26 13:10:38 +0300

    Parse body request

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-// module.exports = async (req, res) => {
-//   const { body } = req;
-//   res.end(`Hello ${body.name}, you just parsed the request body!`);
-// };
+module.exports = async (req, res) => {
+  const { body } = req;
+  res.end(`Hello ${body.name}, you just parsed the request body!`);
+};
 
-const express = require("express");
-const app = express();
-const port = 3000;
+// const express = require("express");
+// const app = express();
+// const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+// app.listen(port, () =>
+//   console.log(`Example app listening at http://localhost:${port}`)
+// );
